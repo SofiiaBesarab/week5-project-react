d8959b28ca5b457c6335f9dd781eabd5cc570f8f
SofiiaBesarab <sofiia.besarab31@gmail.com>
2023-09-13 09:58:43 -0700

    Added GitHub link

--- src/App.js
+++ src/App.js
@@ -2,5 +2,15 @@ import "./App.css";
 import React from "react";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <a
+        href="https://github.com/SofiiaBesarab/week5-project-react"
+        target="_blank"
+      >
+        GitHub
+      </a>
+    </div>
+  );
 }
